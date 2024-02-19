39ac8f7559c0edd47887bd0a45b60caebd9b7034
eleanore-ritter <44507907+eleanore-ritter@users.noreply.github.com>
2024-02-19 09:38:07 -0500

    Rename annotations and investigate chr10 inversion

---
+++ scripts/annotation/rename_annotations_by_chr_ER.sh
@@ -0,0 +1,83 @@
+#!/bin/bash --login
+#SBATCH --time=168:00:00
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --cpus-per-task=1
+#SBATCH --mem=200GB
+#SBATCH --job-name rename_chr
+#SBATCH --output=../job_reports/%x-%j.SLURMout
+
+#Set variables
+GFF="../VvDak_v1/VvDak_v0.1.gff"
+PREFIX="VvDak_v1."
+JUSTIFY="6"
+ZEROS="1"
+OUTPUT="VvDak_v1"
+PROTEINS="../VvDak_v1/VvDak_v0.1_protein.fa"
+TRANSCRIPTS="../VvDak_v1/VvDak_v0.1_transcript.fa"
+
+#Set this variable to the path to wherever you have conda installed
+conda="${HOME}/anaconda3"
+
+#Export paths to conda
+export PATH="${conda}/envs/maker/bin:${PATH}"
+export LD_LIBRARY_PATH="${conda}/envs/maker/lib:${LD_LIBRARY_PATH}"
+
+#Get chromosome list
+cut -f1 ${GFF} | grep -v \# | sort | uniq > chr_list
+
+#Loop over chr_list and rename genes for each chromosome
+mkdir tmp
+cd tmp
+cat ../chr_list | while read line
+do
+	echo "Working on ${line}"
+	if [[ ${line:0:3} = "chr" ]]
+	then
+		awk -v a=${line} '$1==a' ../${GFF} > ${line}.gff
+		if [ $(echo ${line} | wc -c) -gt 4 ]
+		then
+			chr=$(echo ${line} | sed s/chr//)
+		else
+			chr=$(echo ${line} | sed s/chr/0/)
+		fi
+		maker_map_ids \
+		--prefix ${PREFIX}${chr}g \
+		--justify ${JUSTIFY} \
+		--iterate 1 \
+		${line}.gff | awk -v a=${ZEROS} '{if ($2 ~ /-R/) print $0; else print $0a}' | sed s/-R/${ZEROS}./ > ${line}_renamed.map
+	elif [[ CHRUN ]]
+	then
+		awk -v a=${line} '$1==a' ../${GFF} >> chrUN.gff
+	else
+		awk -v a=${line} '$1==a' ../${GFF} > ${line}.gff
+		maker_map_ids \
+			--prefix ${PREFIX}${line}g \
+			--justify ${JUSTIFY} \
+			--iterate 1 \
+			${line}.gff | awk -v a=${ZEROS} '{if ($2 ~ /-R/) print $0; else print $0a}' | sed s/-R/${ZEROS}./ > ${line}_renamed.map
+	fi
+	chr=
+done
+
+if [[ chrUN ]]
+then
+	chr="UN"
+	maker_map_ids \
+		--prefix ${PREFIX}${chr}g \
+		--justify ${JUSTIFY} \
+		--iterate 1 \
+		chrUN.gff | awk -v a=${ZEROS} '{if ($2 ~ /-R/) print $0; else print $0a}' | sed s/-R/${ZEROS}./ > chrUN_renamed.map
+fi
+
+#Combine files
+cd ..
+cat tmp/*_renamed.map > ${OUTPUT}-renamed-genes.map
+rm -R tmp chr_list
+
+#Rename gff & fasta files
+map_gff_ids ${OUTPUT}-renamed-genes.map ${GFF}
+map_fasta_ids ${OUTPUT}-renamed-genes.map ${TRANSCRIPTS}
+map_fasta_ids ${OUTPUT}-renamed-genes.map ${PROTEINS}
+
+echo "Done"
---
+++ scripts/chr10_inv.R
@@ -0,0 +1,75 @@
+#setwd("C:/Users/rittere5/OneDrive - Michigan State University/Dakapo_Genome/") #Work working directory
+setwd("C:/Users/elean/OneDrive - Michigan State University/Dakapo_Genome/")
+
+library(biomaRt)
+library(dplyr)
+library(ggplot2)
+
+########################## PLOT INVERSION ##########################
+# Load data
+hap1.coords <- read.csv("hap1_chr10.coords", skip=4, sep = "\t", header=FALSE)
+colnames(hap1.coords) <- c("S1", "E1", "S2", "E2", "LEN_1", "LEN_2", "PERCENT_ID", "TAGS1", "TAGS2")
+
+# Extract coordinates and set up dataframe
+dak1 <- hap1.coords[,1:2]
+colnames(dak1) <- c("X","Y")
+dak1$genome <- c("Dakapo")
+
+hap1 <- hap1.coords[,4:3]
+colnames(hap1) <- c("X","Y")
+hap1$genome <- c("Rubired haplotype 1")
+
+coords1 <- rbind(dak1,hap1)
+ggplot(coords1, aes(x = X, y = Y, color = genome)) +
+  geom_point()
+
+coords1 <- hap1.coords[,c("E1","E2")]
+ggplot(coords1, aes(x = E1, y = E2)) +
+  geom_point()
+
+########################## FUNCTION OF GENES IN INVERSION ##########################
+# Load data
+dak.ath <- read.csv("Dakapo-Athaliana.tsv", header = FALSE, sep = "\t")
+hap1.genes <- read.csv("chr10-inv-hap1.gene_list.txt", header = FALSE)
+hap2.genes <- read.csv("chr10-inv-hap2.gene_list.txt", header = FALSE)
+
+
+#Add in Arabidopsis orthologs
+data1 <- merge(hap1.genes, dak.ath, by = "V1", all.x = TRUE)
+data2 <- merge(hap2.genes, dak.ath, by = "V1", all.x = TRUE)
+
+#Get TAIR IDs for hap1
+atha.genes1 <- data1$V2
+atha.genes1 <- na.omit(atha.genes1)
+
+ensembl_plants = useMart(biomart="plants_mart", host = "plants.ensembl.org")
+
+datasets <- listDatasets(ensembl_plants)
+ensembl = useDataset("athaliana_eg_gene", ensembl_plants)
+
+results1 <- getBM(attributes=c('ensembl_gene_id','tair_symbol', 'description', 'name_1006', 'tair_locus_model'),
+                  filters = 'tair_locus_model',           
+                  values = atha.genes1, 
+                  mart = ensembl,
+                  uniqueRows = TRUE)
+results1A <- results1[!duplicated(results1$ensembl_gene_id), ] # Remove duplicates
+
+data1A <- merge(data1,results1A, by.x = "V2", by.y = "tair_locus_model", all.x = TRUE)
+
+#Get TAIR IDs for hap2
+atha.genes2 <- data2$V2
+atha.genes2 <- na.omit(atha.genes2)
+
+ensembl_plants = useMart(biomart="plants_mart", host = "plants.ensembl.org")
+
+datasets <- listDatasets(ensembl_plants)
+ensembl = useDataset("athaliana_eg_gene", ensembl_plants)
+
+results2 <- getBM(attributes=c('ensembl_gene_id','tair_symbol', 'description', 'name_1006', 'tair_locus_model'),
+                  filters = 'tair_locus_model',           
+                  values = atha.genes2, 
+                  mart = ensembl,
+                  uniqueRows = TRUE)
+results2A <- results2[!duplicated(results2$ensembl_gene_id), ] # Remove duplicates
+
+data2A <- merge(data2,results2A, by.x = "V2", by.y = "tair_locus_model", all.x = TRUE)
